7f44e9aaac7c95bede71eedc290ecf380b55239b
gold7g <gold.ula4@gmail.com>
2019-10-13 22:29:20 +0200

    Added basic views and html patterns

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
 	name = models.CharField(max_length=200, db_index=True)
@@ -11,6 +12,10 @@ class Category(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse("shop:product_list_by_category", args=[self.slug])
+
 
 
 class Product(models.Model):
@@ -31,5 +36,6 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name
 
-
+	def get_absolute_url(self):
+		return reverse("shop:product_detail", args=[self.id, self.slug])
 	
