3dea283c82f0d7223eed01c6b19f1c95ded24935
gold7g <gold.ula4@gmail.com>
2019-10-13 14:35:18 +0200

    Configured PostgreSQL

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+		'USER': 'shop',
+		'PASSWORD': 'webshop',
     }
 }
 
