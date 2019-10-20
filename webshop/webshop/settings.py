47e052f687c4d0666f4b6fe5a86135fddab75cb7
Daria Lasecka <33495679+Daka123@users.noreply.github.com>
2019-10-20 18:20:46 +0200

    Fixed adding images to products

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CART_SESSION_ID = 'cart'
