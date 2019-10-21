10c6464164a47b1e4457543b158e55b772813684
Daria Lasecka <daria.las@wp.pl>
2019-10-21 23:13:05 +0200

    Created registration, fixed login

--- webshop/account/forms.py
+++ webshop/account/forms.py
@@ -1,5 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
+
 
 class LoginForm(forms.Form):
 	login = forms.CharField()
 	hasło = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+	hasło = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+	hasło2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'email')
+
+	def clean_password2(self):
+		cd = self.cleaned_data
+		if cd['hasło'] != cd['hasło2']:
+			raise forms.ValidationError('Hasła są różne.')
+		return cd['hasło2']
--- webshop/account/urls.py
+++ webshop/account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('login/', views.user_login, name='login'),
+	path('register/', views.register, name='register'),
 ]
