e512f685fd6288168104621bea490162350a98e5
gold7g <gold.ula4@gmail.com>
2019-10-26 17:51:11 +0200

    Login/logout/register feature works, available from the template now

--- webshop/account/urls.py
+++ webshop/account/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	path('login/', views.user_login, name='login'),
 	path('register/', views.register, name='register'),
+	path('logout/', views.user_logout, name='logout'),
+
 ]
