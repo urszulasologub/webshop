7b7ed69bf8974f136ae6390a3fd14b91cf048eed
Urszula Sołogub <34507753+urszulasologub@users.noreply.github.com>
2019-10-27 09:30:17 +0100

    Revert "Redirecting from login panel to main website"

--- webshop/webshop/urls.py
+++ webshop/webshop/urls.py
@@ -18,13 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-	path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 	path('cart/', include('cart.urls', namespace='cart')),
 	path('', include('shop.urls', namespace='shop')),
-	path('account/', include('account.urls')),
-
 ]
+
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
