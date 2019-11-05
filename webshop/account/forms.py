d04347563c5a6956cb74abed2fbc36b3fefbaaaa
Daria Lasecka <daria.las@wp.pl>
2019-11-05 19:11:49 +0100

    Now you can choode date, you don't have to write it

--- webshop/account/forms.py
+++ webshop/account/forms.py
@@ -1,9 +1,7 @@
-from datetime import date
+from datetime import date, datetime
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-
-from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -14,15 +12,37 @@ class LoginForm(forms.Form):
 class UserRegistrationForm(forms.ModelForm):
 	first_name = forms.CharField(label="Imię", max_length=30, help_text='Wymagane.')
 	last_name = forms.CharField(label="Nazwisko", max_length=30, help_text='Wymagane.')
-	email = forms.EmailField(label="Email", max_length=254, help_text='Wymagane.')
-	birthday = forms.DateField(label="Data urodzenia", help_text='Wymagane. Format: YYYY-mm-dd')  # chyba
 
+	DAYS_CHOICES = [(i, str(i)) for i in range(1, 32)]
+	MONTHS_CHOICES = (
+		(1, 'Styczeń'),
+		(2, 'Luty'),
+		(3, 'Marzec'),
+		(4, '4'),  # na razie tyle do tesów
+		(5, '5'),
+		(6, '1'),
+		(7, '1'),
+		(8, '1'),
+		(9, '1'),
+		(10, '1'),
+		(11, '1'),
+		(12, '1'),
+	)
+	YEARS_CHOICES = [(i, str(i)) for i in range(2000, 2100)]  # tymczasowo
+
+	day = forms.TypedChoiceField(label="Dzień", choices=DAYS_CHOICES)
+	month = forms.TypedChoiceField(label="Miesiąc", choices=MONTHS_CHOICES)
+	year = forms.TypedChoiceField(label="Rok", choices=YEARS_CHOICES)
+
+	birthday = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+	email = forms.EmailField(label="Email", max_length=254, help_text='Wymagane.')
 	hasło = forms.CharField(label='Hasło', widget=forms.PasswordInput, validators=[validate_password])
 	hasło2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput, validators=[validate_password])
 
 	class Meta:
 		model = User
-		fields = ('username', 'first_name', 'last_name', 'birthday', 'email')
+		fields = ('username', 'first_name', 'last_name', 'day', 'month', 'year', 'email')
 
 	def clean_hasło2(self):
 		cd = self.cleaned_data
@@ -30,13 +50,26 @@ class UserRegistrationForm(forms.ModelForm):
 			raise forms.ValidationError('Hasła są różne.')
 		return cd['hasło2']
 
-	def clean_birthday(self):
-		birthday = self.cleaned_data['birthday']
-		'''try:
-			birthday = datetime.strptime(date, '%d-%m-%Y')
+	def validate_birthday(self):
+		#birthday = self.cleaned_data['birthday']
+		day = self.cleaned_data['day']
+		month = self.cleaned_data['month']
+		year = self.cleaned_data['year']
+		#self.birthday = year + '-' + month + "-" + day
+		try:
+			self.birthday = datetime(year=int(year), month=int(month), day=int(day))
+			check = True
 		except ValueError:
-			raise forms.ValidationError('Podaj poprawną datę.')
-		'''
-		if date.today() <= birthday:
-			raise forms.ValidationError('Data jest z przyszłości.')
-		return birthday
+			check = False
+		if not check:
+			self.add_error('day', 'Wybierz poprawną datę.')
+			#raise forms.ValidationError('Wybierz poprawną datę.')
+			return "01-01-0001"
+		if datetime.today() <= self.birthday:
+			self.add_error('day', 'Data jest z przyszłości.')
+			#raise forms.ValidationError('Data jest z przyszłości.')
+			return "01-01-0001"
+		return self.birthday
+
+	#birthday = validate_birthday()#forms.CharField(label="Data urodzenia", initial="1969-04-20", widget=forms.HiddenInput())  # , help_text='Wymagane. Format: YYYY-mm-dd'
+
