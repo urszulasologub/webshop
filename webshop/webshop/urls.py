43c60cffd4e5ada5494dfb037816af60b089f78c
gold7g <gold.ula4@gmail.com>
2019-11-11 15:24:17 +0100

    Created checkout, fixes with cart

--- webshop/webshop/urls.py
+++ webshop/webshop/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('cart/', include('cart.urls', namespace='cart')),
+	path('checkout/', include('cart.urls', namespace='cart')),
 	path('', include('shop.urls', namespace='shop')),
 	path('account/', include('account.urls')),
 
