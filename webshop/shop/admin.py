141eae93c41c2313e2b59bcdad564130594c4bd1
Daria Lasecka <daria.las@wp.pl>
2019-11-19 18:42:31 +0100

    Created Parameter and Description models and updated name of Profile

--- webshop/shop/admin.py
+++ webshop/shop/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product, Review
+from .models import Category, Product, Review, Parameter, Description
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ['name', 'slug']
-	prepopulated_fields = {'slug': ('name', )}
+	prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Product)
@@ -12,7 +13,7 @@ class ProductAdmin(admin.ModelAdmin):
 	list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
 	list_filter = ['available', 'created', 'updated']
 	list_editable = ['price', 'available']
-	prepopulated_fields = {'slug': ('name', )}
+	prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Review)
@@ -20,3 +21,14 @@ class ReviewAdmin(admin.ModelAdmin):
 	list_display = ['body', ]
 	search_fields = ['body', ]
 	list_filter = ['rating', ]
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+	list_display = ['category', 'name']
+
+
+@admin.register(Description)
+class DescriptionAdmin(admin.ModelAdmin):
+	list_display = ['parameter', 'product', 'description']
+	list_editable = ['description']
