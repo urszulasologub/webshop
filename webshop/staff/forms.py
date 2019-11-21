1eae489024ab78a1397d6edfb3184593e83d2f25
gold7g <gold.ula4@gmail.com>
2019-11-21 21:27:04 +0100

    Many buttons for staff

---
+++ webshop/staff/forms.py
@@ -0,0 +1,5 @@
+
+from django import forms
+
+class OrderButtons(forms.Form):
+    btn = forms.CharField()
--- webshop/staff/views.py
+++ webshop/staff/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 from cart.models import Order, OrderComponent
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import OrderButtons
+
+def are_components_completed(components):
+	if components != None:
+		for component in components:
+			if component.is_completed == False:
+				return False
+	return True
 
 @staff_member_required
 def manage_orders(request):
 	orders = Order.objects.all()
+	if request.method == 'POST':
+		form = OrderButtons(request.POST)
+		if form.is_valid():
+			val = form.cleaned_data.get("btn")
+			if val == 'Opłacone zamówienia':
+				orders = Order.objects.filter(is_confirmed=True)
+			elif val == 'Skompletowane zamówienia':
+				all_orders = Order.objects.filter()
+				orders = []
+				for order in all_orders:
+					if order.are_products:
+						components = OrderComponent.objects.filter(order=order)
+						if are_components_completed(components):
+							orders.append(order)
+			elif val == 'Wszystkie zamówienia':
+				orders = Order.objects.all()
+			elif val == 'Wysłane zamówienia':
+				orders = Order.objects.filter(is_sent=True)
+	else:
+		form = OrderButtons()
 	return render(request, 'staff/manage_orders.html', {'orders': orders})
 
 
